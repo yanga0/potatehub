f1b62eaf8787c3b149124554860ecc3da23d26df
Yan Gao <yangao@thoughtworks.com>
2015-07-12 23:47:29 +0800

    [gy] initial commits.

---
+++ potatohub/AboutMeViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutMeViewController.h
+//  potatohub
+//
+//  Created by Yan Gao on 7/9/15.
+//  Copyright (c) 2015 ___THOUGHTWORKS___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutMeViewController : UIViewController
+
+@end
---
+++ potatohub/AboutMeViewController.m
@@ -0,0 +1,40 @@
+//
+//  AboutMeViewController.m
+//  potatohub
+//
+//  Created by Yan Gao on 7/9/15.
+//  Copyright (c) 2015 ___THOUGHTWORKS___. All rights reserved.
+//
+
+#import "AboutMeViewController.h"
+
+@interface AboutMeViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *avatar;
+@property (weak, nonatomic) IBOutlet UIButton *clickLogin;
+
+@end
+
+@implementation AboutMeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+@end
